e3861dc79466ec9bd1a640ca33e28a0c1a8d7af6
lekanda <abernaolao@gmail.com>
2021-06-28 16:48:35 +0200

    579.Pestañas con JQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(function(){
     'use strict';
 
-    $('#viajes').css({'display':'block'});
+    $('.nuestros-servicios div:first').show();
     $('.servicios nav a:first').addClass('activo');
 
+    $('.servicios nav a').on('click', mostrarTabs);
+
+    function mostrarTabs() {
+        $('.servicios nav a').removeClass('activo');
+        $(this).addClass('activo');
+        var enlace = $(this).attr('href');
+        // console.log(enlace);
+        $('.nuestros-servicios div').fadeOut();
+        $(enlace).fadeIn();
+        // Para evitar el brinco al presionar un enlace (a)
+        return false;
+    }
 });
